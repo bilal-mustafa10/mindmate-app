db2c3b32795d990e9a14bb891a4ec1ceea1d0815
bilal-mustafa10 <bilalmustafa041@icloud.com>
2023-03-21 03:04:45 +0000

    developed activities page

--- app/screens/(tabs)/ActivitiesScreen.tsx
+++ app/screens/(tabs)/ActivitiesScreen.tsx
@@ -1,25 +1,73 @@
-import {Text, View} from '../../components/Themed';
+import { Text, View, StyleSheet } from 'react-native';
 import * as React from 'react';
-import {useSelector} from 'react-redux';
-import {RootState} from '../../services/redux/store';
-import {styles} from '../../constants/Theme';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../services/redux/store';
+import { styles } from '../../constants/Theme';
+import FastImage from 'react-native-fast-image';
+import {ScrollView} from 'react-native';
 
 export default function ActivitiesScreen() {
-    const {results} = useSelector((state: RootState) => state.activity);
-    
+    const { results } = useSelector((state: RootState) => state.activity);
+
+    const activitiesByTag = {
+        'Keep Active': [],
+        'Give to others': [],
+        'Keep Notice': [],
+        'Connect with others': [],
+        'Keep Learning': [],
+    };
+
+    // Group activities by tag
+    results?.forEach((activity) => {
+        activitiesByTag[activity.five_way_tag].push(activity);
+    });
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Activities</Text>
-            {results !== undefined && results.map((activity, index) => {
-                return (
-                    <View key={index}>
-                        <Text>{activity.title}</Text>
-                    </View>
-                );
-            })
-            }
-        </View>
+        <ScrollView style={styles.container}>
+            {Object.entries(activitiesByTag).map(([tag, activities]) => (
+                <View key={tag}>
+                    <Text style={{ fontWeight: 'bold', marginVertical: 8 }}>{tag}</Text>
+                    <ScrollView
+                        horizontal
+                        contentContainerStyle={style.activitiesContainer}
+                        showsHorizontalScrollIndicator={false}
+                    >
+                        {activities.map((activity, index) => (
+                            <View key={index} style={style.activity}>
+                                <View style={style.activityBox}>
+                                    <FastImage source={{ uri: activity.logo.file }} style={{ width: 40, height: 40 }} />
+                                    <Text style={{fontSize:12, fontFamily:'outfit-semibold'}}>{activity.title}</Text>
+                                </View>
+                            </View>
+                        ))}
+                    </ScrollView>
+                </View>
+            ))}
+        </ScrollView>
     );
 }
 
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    activitiesContainer: {
+        flexDirection: 'row',
+        overflow: 'scroll',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    activity: {
+        marginVertical: 4,
+        marginRight: 8,
+    },
+    activityBox: {
+        width: 140,
+        height: 115,
+        backgroundColor: '#ffffff',
+        padding: 15,
+        borderRadius: 16,
+        marginHorizontal: 4,
+        justifyContent: 'space-between',
+    },
+});
